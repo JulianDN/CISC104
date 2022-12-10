dc228ee9f40aa13e830cf7dd8478fffacd80e1e2
JulianDN <juliannolasco26@gmail.com>
2022-12-09 21:32:37 -0500

    update

---
+++ TurDuckEnAtor/Assets/Scripts/Turkey.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Turkey : Bird
+{
+
+	public Turkey()
+    {
+        throw new NotSupportedException();
+    }
+
+    public Turkey(int WeightInOunces) : base()
+	{
+        base.WeightInOunces = WeightInOunces;
+	}
+
+    public override void RemoveBonesAndAdjustWeight()
+    {
+        if (this.HasBones)
+        {
+            this.WeightInOunces -= (int)((float)this.WeightInOunces * .27);
+            this.HasBones = false;
+        }
+    }
+
+    public override int GetCalories()
+    {
+        return this.WeightInOunces * 54;
+    }
+}
+
